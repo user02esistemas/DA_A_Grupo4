56070f10c69b92dba4eee4835b1fbadbd904b5c3
Gabriel-Ramirez-777 <vasquezfg@uss.edu.pe>
2026-07-13 00:15:59 -0500

    Subiendo estructura base y ConexionBD exitosa - Bloque 1

---
+++ src/main/java/com/movilbus/config/ConexionBD.java
@@ -0,0 +1,42 @@
+package com.movilbus.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    
+    private static ConexionBD instancia;
+    private Connection conexion;
+    
+    private final String url = "jdbc:sqlserver://localhost:1433;databaseName=MovilBusDB;encrypt=true;trustServerCertificate=true;";
+    private final String usuario = "sa";
+    private final String password = "dba";
+
+    private ConexionBD() {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error: No se encontró el driver JDBC de SQL Server: " + e.getMessage());
+        }
+    }
+
+    public static synchronized ConexionBD getInstancia() {
+        if (instancia == null) {
+            instancia = new ConexionBD();
+        }
+        return instancia;
+    }
+
+    public Connection getConexion() {
+        try {
+            if (conexion == null || conexion.isClosed()) {
+                conexion = DriverManager.getConnection(url, usuario, password);
+                System.out.println("¡Conexión establecida con éxito a MovilBusDB!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Error crítico al conectar con SQL Server (verifica credenciales o red): " + e.getMessage());
+        }
+        return conexion;
+    }
+}
